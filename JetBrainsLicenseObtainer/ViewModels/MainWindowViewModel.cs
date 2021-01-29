b822447403beda61556e51803f211e6cae1a94f1
alextnull <alexander.t.null@outlook.com>
2021-01-29 20:36:26 +0700

    SelectedViewModel Property

--- JetBrainsLicenseObtainer/ViewModels/MainWindowViewModel.cs
+++ JetBrainsLicenseObtainer/ViewModels/MainWindowViewModel.cs
@@ -4,5 +4,15 @@ namespace JetBrainsLicenseObtainer.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        #region Selected ViewModel
+        private ViewModelBase _selectedViewModel = new AccountsUCViewModel();
+
+        /// <summary>Selected ViewModel</summary>
+        public ViewModelBase SelectedViewModel
+        {
+            get => _selectedViewModel;
+            set => _selectedViewModel = value; 
+        }
+        #endregion
     }
 }
