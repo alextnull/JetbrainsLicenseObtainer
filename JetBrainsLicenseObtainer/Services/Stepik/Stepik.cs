e990c5a78c0e0e7d50c7516c21c9bf204c14e689
alextnull <alexander.t.null@outlook.com>
2021-01-30 16:05:48 +0700

    Class for interaction with Stepik pages.

---
+++ JetBrainsLicenseObtainer/Services/Stepik/Stepik.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace JetBrainsLicenseObtainer.Services.Stepik
+{
+    class Stepik
+    {
+        #region Fields
+
+        private IWebDriver chromeDriver;
+
+        #endregion
+
+        #region Constructor
+
+        public Stepik()
+        {
+            chromeDriver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory);
+            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        #endregion
+    }
+}
