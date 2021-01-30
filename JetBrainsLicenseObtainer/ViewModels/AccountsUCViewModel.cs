30b9196edaa385dd57884feb7075aa5e2eff2781
alextnull <alexander.t.null@outlook.com>
2021-01-30 13:14:11 +0700

    Account View and ViewModel initial design

--- JetBrainsLicenseObtainer/ViewModels/AccountsUCViewModel.cs
+++ JetBrainsLicenseObtainer/ViewModels/AccountsUCViewModel.cs
@@ -1,8 +1,23 @@
-﻿using JetBrainsLicenseObtainer.ViewModels.Base;
+﻿using JetBrainsLicenseObtainer.Models;
+using JetBrainsLicenseObtainer.ViewModels.Base;
+using System;
+using System.Collections.ObjectModel;
 
 namespace JetBrainsLicenseObtainer.ViewModels
 {
     class AccountsUCViewModel : ViewModelBase
     {
+        public ObservableCollection<Account> Accounts { get; set; }
+
+        public AccountsUCViewModel()
+        {
+            //Just for checking, remove it later
+            Accounts = new ObservableCollection<Account>
+            {
+                new Account("Oleg T", "oleg@gmail.com", "12345", DateTime.Now),
+                new Account("Masha D", "masha@gmail.com", "12345", DateTime.Now),
+                new Account("Dima G", "dimag344@gmail.com", "12345", DateTime.Now)
+            };
+        }
     }
 }
