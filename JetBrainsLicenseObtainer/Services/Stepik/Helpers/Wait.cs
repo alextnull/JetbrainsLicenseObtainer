c9a0bf3268638a7efdb3af13d5766dfea53a082b
alextnull <alexander.t.null@outlook.com>
2021-01-30 15:25:17 +0700

    Class for interacting with registration page on Stepik.

---
+++ JetBrainsLicenseObtainer/Services/Stepik/Helpers/Wait.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace JetBrainsLicenseObtainer.Services.Stepik.Helpers
+{
+    static class Wait
+    {
+        #region Methods
+
+        /// <summary>
+        /// Waits until element becomes visible.
+        ///  Visibility means that the element is not only displayed but also has a height and width that is greater than 0.
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="locator"></param>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        public static IWebElement UntilElementIsVisible(IWebDriver driver, By locator, int seconds = 25)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(seconds)).Until(ExpectedConditions.ElementIsVisible(locator));
+        }
+
+        /// <summary>
+        /// Waits until element loaded.
+        /// This does not necessarily mean that the element is visible.
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="locator"></param>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        public static IWebElement UntilElementIsExist(IWebDriver driver, By locator, int seconds = 25)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(seconds)).Until(ExpectedConditions.ElementExists(locator));
+        }
+
+        #endregion
+    }
+}
---
+++ JetBrainsLicenseObtainer/Services/Stepik/WebsitePages/RegistrationPage.cs
@@ -0,0 +1,69 @@
+﻿using JetBrainsLicenseObtainer.Models;
+using JetBrainsLicenseObtainer.Services.Stepik.Helpers;
+using OpenQA.Selenium;
+
+namespace JetBrainsLicenseObtainer.Services.Stepik.WebsitePages
+{
+    static class RegistrationPage
+    {
+        #region Structures
+
+        private struct Locator
+        {
+            public static readonly By FullnameInput = By.XPath("//input[@id='id_registration_full-name']");
+            public static readonly By EmailInput = By.XPath("//input[@id='id_registration_email']");
+            public static readonly By PasswordInput = By.XPath("//input[@id='id_registration_password']");
+            public static readonly By RegistrateButton = By.XPath("//button[@class='sign-form__btn button_with-loader ']");
+            public static readonly By ProfileButton = By.XPath("//button[@class='navbar__profile-toggler st-button_style_none']");
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Registrate Stepik account
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public static bool Registrate(IWebDriver driver, Account account)
+        {
+            bool isRegistrate;
+
+            try
+            {
+                driver.Navigate().GoToUrl("http://stepik.org/registration");
+                driver.Manage().Window.Maximize();
+                FillInput(driver, Locator.FullnameInput, account.FullName);
+                FillInput(driver, Locator.EmailInput, account.Email);
+                FillInput(driver, Locator.PasswordInput, account.Password);
+                IWebElement registrateButton = Wait.UntilElementIsVisible(driver, Locator.RegistrateButton, 30);
+                registrateButton.Click();
+                Wait.UntilElementIsVisible(driver, Locator.ProfileButton, 30);
+                isRegistrate = true;
+            }
+            catch
+            {
+                isRegistrate = false;
+                driver.Quit();
+            }
+
+            return isRegistrate;
+        }
+
+        /// <summary>
+        /// Finds input using a locator and fill it with text
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="locator"></param>
+        /// <param name="text"></param>
+        private static void FillInput(IWebDriver driver, By locator, string text)
+        {
+            IWebElement element = Wait.UntilElementIsVisible(driver, locator, 20);
+            element.SendKeys(text);
+        }
+
+        #endregion
+    }
+}
