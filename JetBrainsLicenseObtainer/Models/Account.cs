31e49678b3f80577358f1bee2b6d629713c5f45c
alextnull <alexander.t.null@outlook.com>
2021-01-30 11:47:00 +0700

    Account model

---
+++ JetBrainsLicenseObtainer/Models/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JetBrainsLicenseObtainer.Models
+{
+    class Account
+    {
+        #region Properties
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Account(string fullName, string email, string password, DateTime registrationDate)
+        {
+            FullName = fullName;
+            Email = email;
+            Password = password;
+            RegistrationDate = registrationDate;
+        }
+
+        #endregion
+    }
+}
