f5093f21c3c35e146c152f963bea119b9de6e632
alextnull <alexander.t.null@outlook.com>
2021-01-31 14:02:20 +0700

    Input class for interacting with inputs

---
+++ JetBrainsLicenseObtainer/Services/Stepik/Helpers/Input.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace JetBrainsLicenseObtainer.Services.Stepik.Helpers
+{
+    static class Input
+    {
+        /// <summary>
+        /// Finds input using a locator and fill it with text
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="locator"></param>
+        /// <param name="text"></param>
+        public static void Fill(IWebDriver driver, By locator, string text)
+        {
+            IWebElement element = Wait.UntilElementIsVisible(driver, locator, 20);
+            element.SendKeys(text);
+        }
+    }
+}
