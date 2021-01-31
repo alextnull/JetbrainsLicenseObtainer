e9dcc4e0f4565f2887c575257361706207a65f0e
alextnull <alexander.t.null@outlook.com>
2021-01-31 14:17:59 +0700

    Solution model for Stepik.

---
+++ JetBrainsLicenseObtainer/Models/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace JetBrainsLicenseObtainer.Models
+{
+    class Solution
+    {
+        #region Properties
+
+        public string Url { get; private set; }
+        public string AnswerText { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Solution(string url, string answerText)
+        {
+            Url = url;
+            AnswerText = answerText;
+        }
+
+        #endregion
+    }
+}
