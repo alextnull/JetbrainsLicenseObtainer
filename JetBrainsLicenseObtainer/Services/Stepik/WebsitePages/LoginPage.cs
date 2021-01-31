2826b95f056af7d0daf0546c02b6b19c12bdea79
alextnull <alexander.t.null@outlook.com>
2021-01-31 14:07:27 +0700

    Class for interacting with login page on Stepik

---
+++ JetBrainsLicenseObtainer/Services/Stepik/WebsitePages/LoginPage.cs
@@ -0,0 +1,49 @@
+﻿using JetBrainsLicenseObtainer.Services.Stepik.Helpers;
+using OpenQA.Selenium;
+
+namespace JetBrainsLicenseObtainer.Services.Stepik.WebsitePages
+{
+    static class LoginPage
+    {
+        #region Structures
+
+        private struct Locator
+        {
+            public static readonly By EmailInput = By.XPath("//input[@name='login']");
+            public static readonly By PasswordInput = By.XPath("//input[@name='password']");
+            public static readonly By LoginButton = By.XPath("//button[@class='sign-form__btn button_with-loader ']");
+            public static readonly By ProfileButton = By.XPath("//button[@class='navbar__profile-toggler st-button_style_none']");
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static bool Login(IWebDriver driver, string email, string password)
+        {
+            bool isLogin;
+
+            try
+            {
+                driver.Navigate().GoToUrl("https://stepik.org/login");
+                Input.Fill(driver, Locator.EmailInput, email);
+                Input.Fill(driver, Locator.PasswordInput, password);
+
+                IWebElement loginButton = Wait.UntilElementIsVisible(driver, Locator.LoginButton, 30);
+                loginButton.Click();
+                Wait.UntilElementIsVisible(driver, Locator.ProfileButton, 30);
+
+                isLogin = true;
+            }
+            catch
+            {
+                isLogin = false;
+                driver.Quit();
+            }
+
+            return isLogin;
+        }
+
+        #endregion
+    }
+}
