96369a7d316adb680fb9b76d6ab8d8ae32b20187
alextnull <alexander.t.null@outlook.com>
2021-02-03 14:01:22 +0700

    Add constructor

--- JetBrainsLicenseObtainer/Infrastructure/Commands/AsyncRelayCommand.cs
+++ JetBrainsLicenseObtainer/Infrastructure/Commands/AsyncRelayCommand.cs
@@ -8,6 +8,13 @@ namespace JetBrainsLicenseObtainer.Infrastructure.Commands
     {
         private readonly Action<object> _execute;
         private readonly Func<object, bool> _canExecute;
+
+        public AsyncRelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
         public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
 
         public override async Task ExecuteAsync(object parameter) => await Task.Run(() => _execute(parameter));
