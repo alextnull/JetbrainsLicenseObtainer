111da5f1db204614ca51ace0967f227529cdae54
alextnull <alexander.t.null@outlook.com>
2021-02-03 16:09:28 +0700

    Class for database

---
+++ JetBrainsLicenseObtainer/Data/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using JetBrainsLicenseObtainer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JetBrainsLicenseObtainer.Data
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<Account> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=jblodb;Trusted_Connection=True;");
+        }
+    }
+}
