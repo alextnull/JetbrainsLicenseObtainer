4cb3349eb80e3193ccd614f579b154641af6d888
alextnull <alexander.t.null@outlook.com>
2021-02-03 13:59:39 +0700

    AsyncRelayCommand

---
+++ JetBrainsLicenseObtainer/Infrastructure/Commands/AsyncRelayCommand.cs
@@ -0,0 +1,15 @@
+﻿using JetBrainsLicenseObtainer.Infrastructure.Commands.Base;
+using System;
+using System.Threading.Tasks;
+
+namespace JetBrainsLicenseObtainer.Infrastructure.Commands
+{
+    class AsyncRelayCommand : AsyncCommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override async Task ExecuteAsync(object parameter) => await Task.Run(() => _execute(parameter));
+    }
+}
