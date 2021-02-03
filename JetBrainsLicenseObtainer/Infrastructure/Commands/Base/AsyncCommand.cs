95d33124413cb006b656d06891738b3ff456849d
alextnull <alexander.t.null@outlook.com>
2021-02-03 13:48:50 +0700

    Base class for async commands

---
+++ JetBrainsLicenseObtainer/Infrastructure/Commands/Base/AsyncCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace JetBrainsLicenseObtainer.Infrastructure.Commands.Base
+{
+    internal abstract class AsyncCommand : ICommand
+    {
+        private readonly ObservableCollection<Task> runningTasks;
+
+        protected AsyncCommand()
+        {
+            runningTasks = new ObservableCollection<Task>();
+            runningTasks.CollectionChanged += OnRunningTasksChanged;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public IEnumerable<Task> RunningTasks
+        {
+            get => runningTasks;
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute();
+        }
+
+        async void ICommand.Execute(object parameter)
+        {
+            Task runningTask = ExecuteAsync();
+
+            runningTasks.Add(runningTask);
+
+            try
+            {
+                await runningTask;
+            }
+            finally
+            {
+                runningTasks.Remove(runningTask);
+            }
+        }
+
+        public abstract bool CanExecute();
+        public abstract Task ExecuteAsync();
+
+        private void OnRunningTasksChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+}
