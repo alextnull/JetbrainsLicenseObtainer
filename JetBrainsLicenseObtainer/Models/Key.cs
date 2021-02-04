06be8a9fbd9fc7d179bc65acb77d405d8a492124
alextnull <alexander.t.null@outlook.com>
2021-02-04 11:13:54 +0700

    Key model

---
+++ JetBrainsLicenseObtainer/Models/Key.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JetBrainsLicenseObtainer.Models
+{
+    class Key
+    {
+        #region Properties
+
+        public int Id { get; set; }
+        public string LicenseKey { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Key(Account account, string licenseKey, DateTime expirationDate)
+        {
+            Account = account;
+            LicenseKey = licenseKey;
+            ExpirationDate = expirationDate;
+        }
+
+        #endregion
+    }
+}
