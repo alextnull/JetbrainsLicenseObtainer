65c94a1eddf8821a2f47347abd7a2199653c5667
alextnull <alexander.t.null@outlook.com>
2021-02-07 20:33:55 +0700

    Accounts data access

---
+++ JetBrainsLicenseObtainer/Data/AccountsDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using JetBrainsLicenseObtainer.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace JetBrainsLicenseObtainer.Data
+{
+    public class AccountsDataAccess : SqliteDataAccess
+    {
+        public static List<Account> LoadAccounts()
+        {
+            using (IDbConnection db = new SQLiteConnection(_connectionString))
+            {
+                IEnumerable<Account> accounts = db.Query<Account>("select * from Accounts", new DynamicParameters());
+                return accounts.ToList();
+            }
+        }
+
+        public static void SaveAccount(Account account)
+        {
+            using (IDbConnection db = new SQLiteConnection(_connectionString))
+            {
+                db.Execute("insert into Accounts (FullName, Email, Password, RegistrationDate) " +
+                            "values (@FullName, @Email, @Password, @RegistrationDate)", account);
+            }
+        }
+
+        public static void RemoveAccount(Account account)
+        {
+            using (IDbConnection db = new SQLiteConnection(_connectionString))
+            {
+                db.Execute("delete from Accounts where Id = '@Id'", account);
+            }
+        }
+    }
+}
