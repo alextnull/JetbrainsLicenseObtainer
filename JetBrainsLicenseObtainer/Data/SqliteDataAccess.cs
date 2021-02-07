44fad07af0574c4bc8e029e4dafd8f3ed38c8c27
alextnull <alexander.t.null@outlook.com>
2021-02-07 20:33:47 +0700

    Sqlite data access base class

---
+++ JetBrainsLicenseObtainer/Data/SqliteDataAccess.cs
@@ -0,0 +1,9 @@
+﻿using Dapper;
+using JetBrainsLicenseObtainer.Models;
+namespace JetBrainsLicenseObtainer.Data
+{
+    public class SqliteDataAccess
+    {
+        protected static string _connectionString = @"AppDomain.CurrentDomain.BaseDirectory\jblo.db";
+    }
+}
