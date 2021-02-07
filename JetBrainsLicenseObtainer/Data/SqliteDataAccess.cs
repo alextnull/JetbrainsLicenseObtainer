58ebfff330b887bd68572dba23ccbe6411cb759f
alextnull <alexander.t.null@outlook.com>
2021-02-07 21:03:35 +0700

    Fix connection string for sqlite

--- JetBrainsLicenseObtainer/Data/SqliteDataAccess.cs
+++ JetBrainsLicenseObtainer/Data/SqliteDataAccess.cs
@@ -1,9 +1,11 @@
-﻿using Dapper;
-using JetBrainsLicenseObtainer.Models;
+﻿using System;
+using System.IO;
+
 namespace JetBrainsLicenseObtainer.Data
 {
     public class SqliteDataAccess
     {
-        protected static string _connectionString = @"AppDomain.CurrentDomain.BaseDirectory\jblo.db";
+        protected static string _databasePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "jblo.db");
+        protected static string _connectionString = $"Data Source={_databasePath};Version=3";
     }
 }
