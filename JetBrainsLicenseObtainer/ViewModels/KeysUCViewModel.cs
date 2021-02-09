031b75a2e40d0fafe319ccec33ad087c3a0cf712
alextnull <alexander.t.null@outlook.com>
2021-02-09 12:12:21 +0700

    Keys ViewModel - keys collection

--- JetBrainsLicenseObtainer/ViewModels/KeysUCViewModel.cs
+++ JetBrainsLicenseObtainer/ViewModels/KeysUCViewModel.cs
@@ -1,8 +1,30 @@
-﻿using JetBrainsLicenseObtainer.ViewModels.Base;
+﻿using JetBrainsLicenseObtainer.Infrastructure;
+using JetBrainsLicenseObtainer.Models;
+using JetBrainsLicenseObtainer.ViewModels.Base;
 
 namespace JetBrainsLicenseObtainer.ViewModels
 {
     public class KeysUCViewModel : ViewModelBase
     {
+        #region Keys Collection
+
+        AsyncObservableCollection<Key> _keys;
+
+        public AsyncObservableCollection<Key> Keys
+        {
+            get => _keys;
+            set => Set(ref _keys, value);
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public KeysUCViewModel()
+        {
+
+        }
+
+        #endregion
     }
 }
