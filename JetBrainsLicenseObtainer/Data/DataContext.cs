c0389a888e9998191790cfad0de9335bcfca5a5b
alextnull <alexander.t.null@outlook.com>
2021-02-10 12:06:34 +0700

    DataContext class for db

---
+++ JetBrainsLicenseObtainer/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using JetBrainsLicenseObtainer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JetBrainsLicenseObtainer.Data
+{
+    public class DataContext : DbContext
+    { 
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Key> Keys { get; set; }
+        public DbSet<Key> OutdatedKeys { get; set; }
+
+        public DataContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=jblodb;Trusted_Connection=True;");
+        }
+    }
+}
