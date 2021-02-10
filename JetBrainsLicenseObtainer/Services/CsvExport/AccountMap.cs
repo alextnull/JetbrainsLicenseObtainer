74514e0776e74fb0a04b7e05066bc205e043d618
alextnull <alexander.t.null@outlook.com>
2021-02-10 13:19:38 +0700

    CsvHelper maps - fix indexes

--- JetBrainsLicenseObtainer/Services/CsvExport/AccountMap.cs
+++ JetBrainsLicenseObtainer/Services/CsvExport/AccountMap.cs
@@ -9,8 +9,8 @@ namespace JetBrainsLicenseObtainer.Services.CsvExport
         {
             Map(m => m.FullName).Index(0).Name("Full name");
             Map(m => m.Email).Index(1).Name("Email");
-            Map(m => m.Password).Index(1).Name("Password");
-            Map(m => m.RegistrationDate).Index(1).Name("Registration date");
+            Map(m => m.Password).Index(2).Name("Password");
+            Map(m => m.RegistrationDate).Index(3).Name("Registration date");
         }
     }
 }
--- JetBrainsLicenseObtainer/Services/CsvExport/KeyMap.cs
+++ JetBrainsLicenseObtainer/Services/CsvExport/KeyMap.cs
@@ -8,8 +8,8 @@ namespace JetBrainsLicenseObtainer.Services.CsvExport
         public KeyMap()
         {
             Map(m => m.LicenseKey).Index(0).Name("Jetbrains license key");
-            Map(m => m.RegistartionDate).Index(0).Name("Registartion date");
-            Map(m => m.ExpirationDate).Index(0).Name("Expiration date");
+            Map(m => m.RegistartionDate).Index(1).Name("Registartion date");
+            Map(m => m.ExpirationDate).Index(2).Name("Expiration date");
         }
     }
 }
