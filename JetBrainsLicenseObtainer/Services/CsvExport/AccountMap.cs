8dc422b88bd8c708940fdb7e776aac6df9783b26
alextnull <alexander.t.null@outlook.com>
2021-02-10 12:52:09 +0700

    Account map for CsvHelper

---
+++ JetBrainsLicenseObtainer/Services/CsvExport/AccountMap.cs
@@ -0,0 +1,16 @@
+﻿using CsvHelper.Configuration;
+using JetBrainsLicenseObtainer.Models;
+
+namespace JetBrainsLicenseObtainer.Services.CsvExport
+{
+    public class AccountMap : ClassMap<Account>
+    {
+       public AccountMap()
+        {
+            Map(m => m.FullName).Index(0).Name("Full name");
+            Map(m => m.Email).Index(1).Name("Email");
+            Map(m => m.Password).Index(1).Name("Password");
+            Map(m => m.RegistrationDate).Index(1).Name("Registration date");
+        }
+    }
+}
