a7f2219a1f4d719fff4f36b075dbd258eae62846
alextnull <alexander.t.null@outlook.com>
2021-02-12 16:55:11 +0700

    Outdated key model

---
+++ JetBrainsLicenseObtainer/Models/OutdatedKey.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace JetBrainsLicenseObtainer.Models
+{
+    public class OutdatedKey : KeyBase
+    {
+        #region Properties
+
+        public int Id { get; set; }
+        public string LicenseKey { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public OutdatedKey()
+        {
+
+        }
+
+        public OutdatedKey(string licenseKey, DateTime registrationDate, DateTime expirationDate)
+        {
+            LicenseKey = licenseKey;
+            RegistrationDate = registrationDate;
+            ExpirationDate = expirationDate;
+        }
+
+        #endregion
+    }
+}
