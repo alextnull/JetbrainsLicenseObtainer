493659882822529b94090d88d3bb8d54ef3f20e4
alextnull <alexander.t.null@outlook.com>
2021-02-12 18:35:55 +0700

    AboutViewModel - design

--- JetBrainsLicenseObtainer/ViewModels/AboutUCViewModel.cs
+++ JetBrainsLicenseObtainer/ViewModels/AboutUCViewModel.cs
@@ -1,8 +1,44 @@
-﻿using JetBrainsLicenseObtainer.ViewModels.Base;
+﻿using JetBrainsLicenseObtainer.Infrastructure.Commands;
+using JetBrainsLicenseObtainer.ViewModels.Base;
+using System.Diagnostics;
+using System.Windows.Input;
 
 namespace JetBrainsLicenseObtainer.ViewModels
 {
     public class AboutUCViewModel : ViewModelBase
     {
+        #region Commands
+
+        #region OpenGithubPageCommand
+
+        public ICommand OpenGithubPageCommand { get; set; }
+
+        private bool CanOpenGithubPageCommandExecute(object parameter) => true;
+        private void OnOpenGithubPageCommandExecuted(object parameter)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "https://github.com/alextnull/JetBrainsLicenseObtainer",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Constructor
+
+        public AboutUCViewModel()
+        {
+            #region Commands
+
+            OpenGithubPageCommand = new RelayCommand(OnOpenGithubPageCommandExecuted, CanOpenGithubPageCommandExecute);
+
+            #endregion
+        }
+
+        #endregion
     }
 }
